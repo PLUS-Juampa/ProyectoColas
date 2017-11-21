5cbb27511ba5ee8d84e998fcda49b2fe243cc2c9
Juampa <juampa249@gmail.com>
2017-11-21 12:31:18 -0600

    Agregando modelo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,35 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main /* extends Application */ {
+public class Main  extends Application  {
 
-    /*
     @Override
-
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Colas");
         primaryStage.setScene(new Scene(root, 500, 450));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
 
-    */
-
-    public static void main(String[] args){
-        Cola cola = new Cola();
-
-        cola.insertar(5);
-        cola.insertar(10);
-        cola.insertar(20);
-        cola.mostrar();
-        cola.extraer();
-        cola.mostrar();
-        cola.insertar(2);
-        cola.mostrar();
-    }
 }
